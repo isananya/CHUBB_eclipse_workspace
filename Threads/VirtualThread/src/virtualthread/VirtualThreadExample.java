300f395dab6390ed6a67d60c762e2bbff163b3df
isananya <nayakananya2004@gmail.com>
2025-12-05 17:27:32 +0530

    add virtual thread code

---
+++ Threads/VirtualThread/src/virtualthread/VirtualThreadExample.java
@@ -0,0 +1,18 @@
+package virtualthread;
+
+public class VirtualThreadExample {
+    public static void main(String[] args) throws InterruptedException {
+        Thread virtualThread = 
+        Thread.ofVirtual().name("MyVirtualThread").start(() -> {
+            System.out.println("Virtual thread running: " + 
+            Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000); // Simulate some work
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        virtualThread.join(); // Wait for the thread to finish
+        System.out.println("Virtual thread finished.");
+    }
+}
